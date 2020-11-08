c91c93253a0dc057c7ae954d3b9160a8a31173ab
wideeye <chinmay4450@gmail.com>
2020-11-08 21:27:29 +0530

    added files

--- plasmadonation/settings.py
+++ plasmadonation/settings.py
@@ -130,7 +130,8 @@ STATICFILES_DIRS = (
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 
 
